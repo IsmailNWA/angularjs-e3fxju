0f973e68d4d57530ee5406c2a6ae52b4bb3080bb
IsmailNWA <ismail.dinar.nwa@gmail.com>
2022-05-25 13:58:42 +0100

    3 files modified

--- home/home.controller.js
+++ home/home.controller.js
@@ -1,16 +1,20 @@
 import { scopeWatcher } from './scope-watcher';
+import { first } from 'rxjs/operators';
+import { isEqual } from 'lodash';
 class HomeCtrl {
   constructor($scope) {
     'ngInject';
-
+    console.log(isEqual([], []));
     this.name = 'AngularJS';
     this.$scope = $scope;
   }
 
   $onInit() {
-    scopeWatcher(this.$scope, () => this.name).subscribe((value) => {
-      console.log(value);
-    });
+    scopeWatcher(this.$scope, () => this.name)
+      .pipe(first())
+      .subscribe((value) => {
+        console.log(value);
+      });
   }
 }
 
--- home/scope-watcher.js
+++ home/scope-watcher.js
@@ -1,13 +1,19 @@
 import { Subject } from 'rxjs';
+import { finalize, distinctUntilChanged } from 'rxjs/operators';
+import {isEqual} from 'lodash';
 
 export const scopeWatcher = ($scope, propToWatch) => {
   const subject = new Subject();
   const unregisterWatcher = $scope.$watch(propToWatch, (newValue, oldValue) => {
-    subject.next({ newValue, oldValue });
+    if(!isEqual(newValue, oldValue)) {
+      subject.next({ newValue, oldValue });
+    }
   });
 
   return subject.asObservable().pipe(
+    distinctUntilChanged(isEqual),
     finalize(() => {
+      console.log('unsunbscribe')
       unregisterWatcher();
     })
   );
